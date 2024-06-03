825ceb7c64a0b7099e46ca79bba029bf70ac6cc1
Strange4 <83730323+Strange4@users.noreply.github.com>
2024-06-03 14:48:40 -0400

    make the svg a template

--- clock_face/SVGWriter.go
+++ clock_face/SVGWriter.go
@@ -1,47 +1,63 @@
 package clock_face
 
 import (
-	"fmt"
 	"io"
+	"text/template"
 )
 
+type svgClockData struct {
+	Center     Point
+	SecondHand Point
+	MinuteHand Point
+	HourHand   Point
+	Radius     float64
+	Width      float64
+	Height     float64
+}
+
+const svgTemplate = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 {{printf "%.3f" .Width}} {{printf "%.3f" .Height}}"
+     version="2.0">
+
+  <!-- bezel -->
+  <circle id="bezel" cx="{{printf "%.3f" .Center.X}}" cy="{{printf "%.3f" .Center.Y}}" r="{{printf "%.3f" .Radius}}" style="fill:#fff;stroke:#000;stroke-width:5px;"/>
+
+  <!-- hour hand -->
+  <line id="hour_hand" x1="{{printf "%.3f" .Center.X}}" y1="{{printf "%.3f" .Center.Y}}" x2="{{printf "%.3f" .HourHand.X}}" y2="{{printf "%.3f" .HourHand.Y}}"
+        style="fill:none;stroke:#000;stroke-width:7px;"/>
+
+  <!-- minute hand -->
+  <line id="minute_hand" x1="{{printf "%.3f" .Center.X}}" y1="{{printf "%.3f" .Center.Y}}" x2="{{printf "%.3f" .MinuteHand.X}}" y2="{{printf "%.3f" .MinuteHand.Y}}"
+        style="fill:none;stroke:#000;stroke-width:7px;"/>
+
+  <!-- second hand -->
+  <line id="second_hand" x1="{{printf "%.3f" .Center.X}}" y1="{{printf "%.3f" .Center.Y}}" x2="{{printf "%.3f" .SecondHand.X}}" y2="{{printf "%.3f" .SecondHand.Y}}"
+        style="fill:none;stroke:#f00;stroke-width:3px;"/>
+</svg>`
+
 func SVGWriter(writer io.Writer, clock *Clock, center Point, radius float64) {
-	writer.Write([]byte(svgStart))
-	writeSecondHand(writer, clock, center, radius)
-	writeMinuteHand(writer, clock, center, radius)
-	writeHourHand(writer, clock, center, radius)
-	writer.Write([]byte(svgEnd))
-}
-
-func writeSecondHand(writer io.Writer, clock *Clock, center Point, radius float64) {
+	tmpl, err := template.New("svg_clock.svg").Parse(svgTemplate)
+	if err != nil {
+		panic(err)
+	}
 	secondHandLength := radius * 0.6
-	secondHand := unitPointToHandPoint(secondHandLength, clock.SecondHand(), center)
-
-	fmt.Fprintf(writer, `
-	<!-- second hand -->
-	<line id="second_hand" x1="%.3f" y1="%.3f" x2="%.3f" y2="%.3f"
-		  style="fill:none;stroke:#f00;stroke-width:3px;"/>
-		  `, center.X, center.Y, secondHand.X, secondHand.Y)
-}
-
-func writeMinuteHand(writer io.Writer, clock *Clock, center Point, radius float64) {
 	minuteHandLength := radius * 0.5
-	minuteHand := unitPointToHandPoint(minuteHandLength, clock.MinuteHand(), center)
-	fmt.Fprintf(writer, `
-	<!-- minute hand -->
-	<line id="minute_hand" x1="%.3f" y1="%.3f" x2="%.3f" y2="%.3f"
-		style="fill:none;stroke:#000;stroke-width:7px;"/>
-	`, center.X, center.Y, minuteHand.X, minuteHand.Y)
-}
-
-func writeHourHand(writer io.Writer, clock *Clock, center Point, radius float64) {
 	hourHandLength := radius * 0.3
-	hourHand := unitPointToHandPoint(hourHandLength, clock.HourHand(), center)
-	fmt.Fprintf(writer, `
-	<!-- hour hand -->
-	<line id="hour_hand" x1="%.3f" y1="%.3f" x2="%.3f" y2="%.3f"
-		style="fill:none;stroke:#000;stroke-width:7px;"/>
-	`, center.X, center.Y, hourHand.X, hourHand.Y)
+
+	data := svgClockData{
+		Center:     center,
+		SecondHand: unitPointToHandPoint(secondHandLength, clock.SecondHand(), center),
+		MinuteHand: unitPointToHandPoint(minuteHandLength, clock.MinuteHand(), center),
+		HourHand:   unitPointToHandPoint(hourHandLength, clock.HourHand(), center),
+	}
+	err = tmpl.Execute(writer, data)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func unitPointToHandPoint(handLength float64, unitPoint, center Point) Point {
