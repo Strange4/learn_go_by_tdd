b7370938b9a6a0d66d1a6858cf2d8ea17960d2de
Strange4 <83730323+Strange4@users.noreply.github.com>
2024-06-03 14:48:31 -0400

    add readme, add default name to hello, add assertString helper

---
+++ README.md
@@ -0,0 +1,5 @@
+# Writing and Learning Go by TDD
+
+This repo is made to have a basic understanding of go and the features of the language
+
+It follows the good resouce of [Learn Go with tests](https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world#declaring-variables)
--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello, Me."
-	got := Hello("Me")
+	t.Run("With a person to name", func(t *testing.T) {
+		want := "Hello, Me"
+		got := Hello("Me")
+		assertString(t, got, want)
+	})
+	t.Run("Default name when none is provided", func(t *testing.T) {
+		want := "Hello, World"
+		got := Hello("")
+		assertString(t, got, want)
+	})
+}
+
+func assertString(t *testing.T, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("Hello() = %q, want %q", got, want)
 	}
