dda5ae5bd010a9ecbf8f29d2bf58f5ff9f000089
Strange4 <83730323+Strange4@users.noreply.github.com>
2024-06-03 14:48:42 -0400

    refactor parsed templates & add top and bottom html

--- blog_renderer/blog_renderer.go
+++ blog_renderer/blog_renderer.go
@@ -10,13 +10,21 @@ import (
 //go:embed "templates/*"
 var postTemplates embed.FS
 
-func Render(writer io.Writer, post *blogpost.Post) error {
+type PostRenderer struct {
+	tmpl *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
 	tmpl, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
-		return err
+		return nil, err
 	}
+	return &PostRenderer{tmpl}, nil
+}
 
-	if err = tmpl.Execute(writer, post); err != nil {
+func (p *PostRenderer) Render(writer io.Writer, post *blogpost.Post) error {
+
+	if err := p.tmpl.ExecuteTemplate(writer, "postTemplate.gohtml", post); err != nil {
 		return err
 	}
 
--- blog_renderer/blog_renderer_test.go
+++ blog_renderer/blog_renderer_test.go
@@ -2,20 +2,50 @@ package blog_renderer_test
 
 import (
 	"bytes"
+	"hello/assertions"
 	blogpost "hello/blog"
 	"hello/blog_renderer"
 	"strings"
 	"testing"
 )
 
-const wantedHTML = `<h1>How to make real time graphics renderer</h1>
+const wantedHTML = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My amazing blog!</title>
+    <meta charset="UTF-8"/>
+    <meta name="description" content="oh no i am under the water, please help me" lang="en"/>
+</head>
+<body>
+<nav role="navigation">
+    <div>
+        <h1>Strange's blog</h1>
+        <ul>
+            <li><a href="/">home</a></li>
+            <li><a href="about">about</a></li>
+            <li><a href="archive">archive</a></li>
+        </ul>
+    </div>
+</nav>
+<main>
+
+<h1>How to make real time graphics renderer</h1>
 <p>this is not a real advice</p>
 Tags:
 <ul>
     <li>gpu</li>
     <li>shader</li>
     <li>BRDF</li>
-</ul>`
+</ul>
+
+</main>
+<footer>
+    <ul>
+        <li><a href="https://github.com/Strange4">GitHub</a></li>
+    </ul>
+</footer>
+</body>
+</html>`
 
 func TestRender(t *testing.T) {
 	post := blogpost.Post{
@@ -26,13 +56,15 @@ func TestRender(t *testing.T) {
 	}
 
 	buf := bytes.Buffer{}
-	err := blog_renderer.Render(&buf, &post)
+	renderer, err := blog_renderer.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = renderer.Render(&buf, &post)
 	if err != nil {
 		t.Fatal(err)
 	}
 	got := strings.ReplaceAll(buf.String(), "\r", "")
 	want := wantedHTML
-	if want != got {
-		t.Errorf("wanted post '%q' but only got '%q'", want, got)
-	}
+	assertions.AssertString(t, got, want)
 }
